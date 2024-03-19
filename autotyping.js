ae7100c59e0c6d4ecb7d48c4bc93e3c3bad02403
irakozemarie <irakoze.antonny@gmail.com>
2024-03-19 22:55:15 +0200

    new edition of commit

--- autotyping.js
+++ autotyping.js
@@ -119,7 +119,7 @@ var AutoTyping = (function (e) {
 ]).default;
 
 
-const exampleText = ['Designer',' Developer', ];
+const exampleText = ['Frontend Developer',' UI/UX Designer', ];
 const exampleTyping = new AutoTyping('#text', exampleText, {
     typeSpeed: 100,
     deleteSpeed: 100,
